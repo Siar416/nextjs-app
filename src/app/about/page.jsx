8207877c8cc82f4de5daeaa07120fae41f998e34
Siar Wahidi <siar.wahidi@gmail.com>
2024-04-23 17:55:23 -0400

    added some pages to public folder along with allowing for external images within next config file

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/9056224/pexels-photo-9056224.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="about"
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
