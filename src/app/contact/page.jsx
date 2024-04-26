b0041d676418ea3e3336c810700081888d0b67b9
Siar Wahidi <siar.wahidi@gmail.com>
2024-04-25 20:19:42 -0400

    added content and form along with styling for contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,33 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="about" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" name="name" placeholder="First and Last Name" />
+          <input type="text" name="email" placeholder="Email Address" />
+          <input
+            type="text"
+            name="number"
+            placeholder="Phone Number (Optional)"
+          />
+          <textarea
+            name="message"
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
